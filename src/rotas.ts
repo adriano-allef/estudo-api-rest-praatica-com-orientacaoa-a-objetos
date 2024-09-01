7e0f6ebbb76b66197b01e50c28cd18516398742f
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-09-01 12:23:47 -0300

    Implementação do cadastrar autor

--- src/rotas.ts
+++ src/rotas.ts
@@ -7,6 +7,8 @@ const rotas = Router()
 const autorControlador = new AutorControlador()
 
 rotas.get('/autores', autorControlador.listar)
+rotas.get('/autores/:id', autorControlador.detalhar)
+rotas.post('/autores', autorControlador.cadastrar)
 
 
 
